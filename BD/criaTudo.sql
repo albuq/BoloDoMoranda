970e0fd515d7d0b15710748179866423a03c32f6
albuq <barbara.albuquerque@usp.br>
2016-06-17 03:45:12 -0300

    revisao modelos + cria tabelas

---
+++ BD/criaTudo.sql
@@ -0,0 +1,322 @@
+﻿-- Database Section
+-- ________________ 
+
+create database BoloDoMoranda;
+--drop schema public cascade;
+--create schema public;
+
+-- DBSpace Section
+-- _______________
+
+
+-- Tables Section
+-- _____________ 
+
+create table BOLO (
+     ID_bolo SERIAL not null,
+     Nome varchar(100) not null,
+     Sabor varchar(50) not null,
+     Mensagem varchar(50),
+     Descricao varchar(300),
+     Ingredientes varchar(500) not null,
+     Imagem varchar(200)[] not null,
+     primary key (ID_bolo));
+
+create table BOLO_PASTA (
+     ID_bolo SERIAL not null,
+     primary key (ID_bolo));
+
+create table BOLO_CASAMENTO (
+     ID_bolo SERIAL not null,
+     primary key (ID_bolo));
+
+create table BOLO_TRADICIONAL (
+     ID_bolo SERIAL not null,
+     primary key (ID_bolo));
+
+create table CLIENTE (
+     CPF numeric(11) not null,
+     Nome varchar(100) not null,
+     Telefone numeric(10) not null,
+     Email varchar(100) not null,
+     Username varchar(50) not null,
+     Senha varchar(20) not null,
+     ID_venda integer not null,
+     ID_END integer not null,
+     primary key (CPF));
+
+create table ENDERECO (
+     ID_END SERIAL not null,
+     Logradouro varchar(100) not null,
+     Numero integer not null,
+     Complemento varchar(20),
+     Bairro varchar(30) not null,
+     Cidade varchar(50) not null,
+     Estado varchar(20) not null,
+     Pais varchar(30) not null,
+     CEP numeric(8) not null,
+     primary key (ID_END));
+
+create table INFO_NUTRICIONAIS (
+     ID_bolo integer not null,
+     valor_energetico integer not null,
+     carboidratos integer not null,
+     proteinas integer not null,
+     gorduras_totais integer not null,
+     gorduras_saturadas integer not null,
+     gorduras_trans integer not null,
+     fibra_alimentar integer not null,
+     sodio integer not null,
+     primary key (ID_bolo));
+
+create table REF_INFO_NUTRICIONAIS (
+     valor_energetico integer not null,
+     carboidratos integer not null,
+     proteinas integer not null,
+     gorduras_totais integer not null,
+     gorduras_saturadas integer not null,
+     gorduras_trans integer not null,
+     fibra_alimentar integer not null,
+     sodio integer not null);
+
+create table PGTO_BOLETO (
+     ID_pgto SERIAL not null,
+     Data_Vencimento date not null,
+     Numero_Boleto integer not null,
+     primary key (ID_pgto));
+
+create table avalia (
+     ID_bolo SERIAL not null,
+     CPF numeric(11) not null,
+     Nota numeric(2,2) not null,
+     Comentario varchar(140),
+     primary key (ID_bolo, CPF));
+
+create table FORMA_PGTO (
+     ID_pgto integer not null,
+     ID_venda integer not null,
+     primary key (ID_pgto),
+     unique (ID_venda));
+
+create table BOLO_CHA (
+     ID_bolo SERIAL not null,
+     primary key (ID_bolo));
+
+
+create table compoe (
+     ID_bolo integer not null,
+     porcao numeric(2,2) not null,
+     primary key (ID_bolo));
+
+create table constitue (
+     ID_bolo integer not null,
+     ID_venda integer not null,
+     porcao numeric(2,2) not null,
+     preco money not null,
+     primary key (ID_bolo, ID_venda));
+
+CREATE TYPE periodo_entrega AS ENUM ('manha', 'tarde', 'noite');
+
+create table entrega (
+     ID_ent SERIAL not null,
+     ID_END integer not null,
+     Age_Data_Agendada date not null,
+     Age_Periodo_Agendado periodo_entrega not null,
+     ID_venda integer not null,
+     primary key (ID_ent),
+     unique (ID_END));
+
+create table PGTO_CARTAO (
+     ID_pgto integer not null,
+     Numero_Cartao numeric(16) not null,
+     Nome_Titular varchar(100) not null,
+     Cod_Seguranca numeric(4) not null,
+     Val_Mes_Validade integer not null, --integer pq date seria desnecessario
+     Val_Ano_Validade integer not null,
+     Par_Qtd_Parcela integer not null,
+     Par_Valor_Parcela money not null,
+     primary key (ID_pgto));
+
+
+create table VENDA (
+     ID_venda SERIAL not null,
+     Data_Cadastro date not null,
+     Data_Confirmacao date,
+     Valor_Total money not null,
+     Pgto_Conf boolean not null,
+     Empacotamento_Conf boolean not null,
+     Despache_Conf boolean not null,
+     Entrega_Conf boolean not null,
+     primary key (ID_venda));
+
+
+-- Constraints Section
+-- ___________________ 
+
+
+alter table BOLO add constraint ID_BOLO_CHK
+     check(exists(select * from compoe
+                  where compoe.ID_bolo = ID_bolo)); 
+
+alter table BOLO add constraint ID_BOLO_CHK
+     check(exists(select * from constitue
+                  where constitue.ID_bolo = ID_bolo)); 
+
+alter table BOLO_PASTA add constraint ID_BOLO__BOLO_3_FK
+     foreign key (ID_bolo)
+     references BOLO;
+
+alter table BOLO_CASAMENTO add constraint ID_BOLO__BOLO_2_FK
+     foreign key (ID_bolo)
+     references BOLO;
+
+alter table BOLO_TRADICIONAL add constraint ID_BOLO__BOLO_1_FK
+     foreign key (ID_bolo)
+     references BOLO;
+
+alter table CLIENTE add constraint REF_CLIEN_VENDA_FK
+     foreign key (ID_venda)
+     references VENDA;
+
+alter table CLIENTE add constraint EQU_CLIEN_ENDER_FK
+     foreign key (ID_END)
+     references ENDERECO;
+
+alter table ENDERECO add constraint ID_ENDERECO_CHK
+     check(exists(select * from CLIENTE
+                  where CLIENTE.ID_END = ID_END)); 
+
+alter table ENDERECO add constraint ID_ENDERECO_CHK
+     check(exists(select * from entrega
+                  where entrega.ID_END = ID_END)); 
+
+alter table INFO_NUTRICIONAIS add constraint ID_INFO__compo_FK
+     foreign key (ID_bolo)
+     references compoe;
+
+alter table PGTO_BOLETO add constraint ID_PGTO__FORMA_1_FK
+     foreign key (ID_pgto)
+     references FORMA_PGTO;
+
+alter table avalia add constraint REF_avali_CLIEN_FK
+     foreign key (CPF)
+     references CLIENTE;
+
+alter table avalia add constraint REF_avali_BOLO
+     foreign key (ID_bolo)
+     references BOLO;
+
+alter table FORMA_PGTO add constraint SID_FORMA_VENDA_FK
+     foreign key (ID_venda)
+     references VENDA;
+
+alter table BOLO_CHA add constraint ID_BOLO__BOLO_FK
+     foreign key (ID_bolo)
+     references BOLO;
+
+alter table compoe add constraint ID_compo_BOLO_CHK
+     check(exists(select * from INFO_NUTRICIONAIS
+                  where INFO_NUTRICIONAIS.ID_bolo = ID_bolo)); 
+
+alter table compoe add constraint ID_compo_BOLO_FK
+     foreign key (ID_bolo)
+     references BOLO;
+
+alter table constitue add constraint REF_const_VENDA_FK
+     foreign key (ID_venda)
+     references VENDA;
+
+alter table constitue add constraint EQU_const_BOLO
+     foreign key (ID_bolo)
+     references BOLO;
+
+alter table entrega add constraint REF_entre_VENDA_FK
+     foreign key (ID_venda)
+     references VENDA;
+
+alter table entrega add constraint SID_entre_ENDER_FK
+     foreign key (ID_END)
+     references ENDERECO;
+
+alter table PGTO_CARTAO add constraint ID_PGTO__FORMA_FK
+     foreign key (ID_pgto)
+     references FORMA_PGTO;
+
+alter table VENDA add constraint ID_VENDA_CHK
+     check(exists(select * from FORMA_PGTO
+                  where FORMA_PGTO.ID_venda = ID_venda)); 
+
+
+-- Index Section
+-- _____________ 
+
+create unique index ID_BOLO_IND
+     on BOLO (ID_bolo);
+
+create unique index ID_BOLO__BOLO_3_IND
+     on BOLO_PASTA (ID_bolo);
+
+create unique index ID_BOLO__BOLO_2_IND
+     on BOLO_CASAMENTO (ID_bolo);
+
+create unique index ID_BOLO__BOLO_1_IND
+     on BOLO_TRADICIONAL (ID_bolo);
+
+create unique index ID_CLIENTE_IND
+     on CLIENTE (CPF);
+
+create index REF_CLIEN_VENDA_IND
+     on CLIENTE (ID_venda);
+
+create index EQU_CLIEN_ENDER_IND
+     on CLIENTE (ID_END);
+
+create unique index ID_ENDERECO_IND
+     on ENDERECO (ID_END);
+
+create unique index ID_INFO__compo_IND
+     on INFO_NUTRICIONAIS (ID_bolo);
+
+create unique index ID_PGTO__FORMA_1_IND
+     on PGTO_BOLETO (ID_pgto);
+
+create unique index ID_avalia_IND
+     on avalia (ID_bolo, CPF);
+
+create index REF_avali_CLIEN_IND
+     on avalia (CPF);
+
+create unique index ID_FORMA_PGTO_IND
+     on FORMA_PGTO (ID_pgto);
+
+create unique index SID_FORMA_VENDA_IND
+     on FORMA_PGTO (ID_venda);
+
+create unique index ID_BOLO__BOLO_IND
+     on BOLO_CHA (ID_bolo);
+
+
+create unique index ID_compo_BOLO_IND
+     on compoe (ID_bolo);
+
+create unique index ID_constitue_IND
+     on constitue (ID_bolo, ID_venda);
+
+create index REF_const_VENDA_IND
+     on constitue (ID_venda);
+
+create unique index ID_entrega_IND
+     on entrega (ID_ent);
+
+create index REF_entre_VENDA_IND
+     on entrega (ID_venda);
+
+create unique index SID_entre_ENDER_IND
+     on entrega (ID_END);
+
+create unique index ID_PGTO__FORMA_IND
+     on PGTO_CARTAO (ID_pgto);
+
+create unique index ID_VENDA_IND
+     on VENDA (ID_venda);
+
